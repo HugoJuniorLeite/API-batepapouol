f09d87f4a457b2b975aa4ea51495684fba22c05b
Hugo Junior <hjunioreng@gmail.com>
2023-01-14 18:46:38 -0300

    schema messages

--- src/app.js
+++ src/app.js
@@ -81,7 +81,19 @@ app.post("/messages",async (req,res)=>{
 const {to, text,type } = req.body
 const {user} =req.headers
 
-try{ 
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required()
+})
+try{
+    
+    
+    const validation = messageSchema.validate({to, text, type}, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
 
     await db.collection("messages").insertOne({
         
