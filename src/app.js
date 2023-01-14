8dc8930558c3f6989691cad7d312399dd781ada9
Hugo Junior <hjunioreng@gmail.com>
2023-01-14 15:10:58 -0300

    adicionando o get participants

--- src/app.js
+++ src/app.js
@@ -11,36 +11,52 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient (process.env.DATABASE_URL)
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db
 
 
 mongoClient.connect()
-.then(()=>{
-    db = mongoClient.db()
-})
+    .then(() => {
+        db = mongoClient.db()
+    })
 
+app.get("/participants"),async (req,res) =>{
 
-app.post("/participants",async (req, res) => {
-    const {name} = req.body;
-    
-    try{
-   //     const user = await db.collection('participants').findOne({name:name}) 
-     //   if(user){return res.status(422).send("usuario já existe na sala")}
+try{
+const participants = await db.collection("/participants").find().toArray();
+res.send(participants)
 
-        if(!name) return  res.status(422).send("O campo nome é obrigatorio")
-    
- await	db.collection("/participants").insertOne({
-		name: name,
-		lastStatus: Date.now()
-	})
-    
-    return res.sendStatus(201)
-	} catch(err){return res.status(500).send(err.message)}
+}catch(err){return res.status(500).send(err.message)}
+
+}
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+
+    try {
+        //     const user = await db.collection('participants').findOne({name:name}) 
+        //   if(user){return res.status(422).send("usuario já existe na sala")}
+
+        if (!name || typeof name !== "string") return res.status(422).send("O campo nome é obrigatorio")
+
+        await db.collection("/participants").insertOne({
+            name: name,
+            lastStatus: Date.now()
+        })
+
+        await db.collection("/message").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        })
+        return res.sendStatus(201)
+    } catch (err) { return res.status(500).send(err.message) }
 });
 
 
 const PORT = process.env.PORT_SEVER
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`servidor rodando na porta ${PORT}`)
 })
