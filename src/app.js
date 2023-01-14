46d47aa9b3801d11f3539a01716fb6314236c75f
Hugo Junior <hjunioreng@gmail.com>
2023-01-13 23:20:15 -0300

    adicionando o post participants

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
-import dotenv from 'dotenv'
+import dayjs from 'dayjs'
+
 dotenv.config();
 
 const app = express();
@@ -9,16 +11,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient (process.env.DATABASE_URL);
+const mongoClient = new MongoClient (process.env.DATABASE_URL)
 let db
 
 
 mongoClient.connect()
 .then(()=>{
-    db = mongoClient.db();
+    db = mongoClient.db()
 })
 
-//.catch()
+
+app.post("/participants",async (req, res) => {
+    const {name} = req.body;
+    
+    try{
+   //     const user = await db.collection('participants').findOne({name:name}) 
+     //   if(user){return res.status(422).send("usuario já existe na sala")}
+
+        if(!name) return  res.status(422).send("O campo nome é obrigatorio")
+    
+ await	db.collection("/participants").insertOne({
+		name: name,
+		lastStatus: Date.now()
+	})
+    
+    return res.sendStatus(201)
+	} catch(err){return res.status(500).send(err.message)}
+});
+
 
 const PORT = process.env.PORT_SEVER
 app.listen(PORT,()=>{
